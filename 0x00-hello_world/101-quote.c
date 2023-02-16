5c4f77b3fd0d47a48fa999a07f8f985bfab0e327
Natnael Hailu <122819655+nati673@users.noreply.github.com>
2023-02-16 14:46:03 -0800

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,8 +1,12 @@
 #include <unistd.h>
-
+/*
+ * main - Entry point for the program
+ *
+ * Return: 1 
+ */
 int main(void)
 {
-	    char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	        write(2, message, 59);
-		    return 1;
+char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+write(2, message, 59);
+return (1);
 }
