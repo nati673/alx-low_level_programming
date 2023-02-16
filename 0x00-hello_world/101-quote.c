5b9aa54db186c09f5b7b2fd00c6cf1adeae98c31
Natnael Hailu <122819655+nati673@users.noreply.github.com>
2023-02-16 14:54:45 -0800

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,6 @@
 #include <unistd.h>
-/* This is the main function that prints a quote */
+/* This is the main function that prints a quote 
+*/
 int main(void)
 {
   char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
