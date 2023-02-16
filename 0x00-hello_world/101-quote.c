c6b72b37f0e223eae44cc671fe160c19bff2bff7
Natnael Hailu <122819655+nati673@users.noreply.github.com>
2023-02-16 14:50:11 -0800

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,11 +2,11 @@
 /*
  * main - Entry point for the program
  *
- * Return: 1 
+ * Return: 1
  */
 int main(void)
 {
-char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+  char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 write(2, message, 59);
 return (1);
 }
