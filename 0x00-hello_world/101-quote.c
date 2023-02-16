c7201b896806158f9cc6d5cb260b59b48c95200e
Natnael Hailu <122819655+nati673@users.noreply.github.com>
2023-02-16 15:16:54 -0800

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -9,6 +9,6 @@
 int main(void)
 {
 char *str = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-    write(STDERR_FILENO, str, 59);
+write(STDERR_FILENO, str, 59);
     return (1);
 }
