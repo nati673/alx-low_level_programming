dee8a81101d9db860bb0973f586010a160cab973
Natnael Hailu <122819655+nati673@users.noreply.github.com>
2023-02-16 13:58:40 -0800

    Update 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,15 @@
 #include <stdio.h>
-
+/**
+ * main - Entry point for the program
+ *
+ * This function is the entry point for the program. It performs the following
+ * tasks:
+ *  - Initializes variables
+ *  - Calls other functions
+ *  - Prints output to the console
+ *
+ * Return: 0 on success, non-zero value on error
+ */
 int main(void)
 {
 	    printf("with proper grammar, but the outcome is a piece of art,\n");
