3966f3cd59e5258af0086141a4efdd0db7d75772
Natnael Hailu <122819655+nati673@users.noreply.github.com>
2023-02-21 13:48:52 -0800

    Update 103-fibonacci.c

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -1,25 +1,25 @@
 #include <stdio.h>
 
 /**
- *main - Prints the add of the Fibonacci numbers
+ *main - Computes the sum of the first 50 Fibonacci numbers
  *
  * Return: Always 0.
  */
 int main(void)
 {
-	int c;
-	long int n1, n2, fn;
+    int c;
+    long int n1, n2, fn, sum;
 
-	n1 = 1;
-	n2 = 2;
-	printf("%ld, %ld", n1, n2);
-	for (c = 0; c < 48; c++)
-	{
-		fn = n1 + n2;
-		printf(", %ld", fn);
-		n1 = n2;
-		n2 = fn;
-	}
-	printf("\n");
-	return (0);
+    n1 = 1;
+    n2 = 2;
+    sum = n1 + n2;
+    for (c = 0; c < 48; c++)
+    {
+        fn = n1 + n2;
+        sum += fn;
+        n1 = n2;
+        n2 = fn;
+    }
+    printf("%ld\n", sum);
+    return (0);
 }
