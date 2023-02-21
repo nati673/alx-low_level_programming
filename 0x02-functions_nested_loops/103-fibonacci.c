07de41c880f61cb89714d1e940ff83af4ee46b15
Natnael Hailu <122819655+nati673@users.noreply.github.com>
2023-02-21 13:53:58 -0800

    Update 103-fibonacci.c

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -1,26 +1,35 @@
 #include <stdio.h>
 
-/**
- * main - Prints the sum of even Fibonacci numbers below 4,000,000
- *
- * Return: Always 0.
- */
-int main(void)
-{
-    int i;
-    long int f1 = 1, f2 = 2, fn, sum = 0;
+	/**
+	 * main - finds and prints the sum of the even-valued terms
+	 * followed by a new line
+	 * Return: Always 0 (Success)
+	 */
+	int main(void)
+	{
+		int i;
+		unsigned long int j, k, next, sum;
 
-    while (f1 <= 4000000)
-    {
-        if (f1 % 2 == 0)
-            sum += f1;
 
-        fn = f1 + f2;
-        f1 = f2;
-        f2 = fn;
-    }
+		j = 1;
+		k = 2;
+		sum = 0;
 
-    printf("%ld\n", sum);
 
-    return (0);
-}
+		for (i = 1; i <= 33; ++i)
+		{
+			if (j < 4000000 && (j % 2) == 0)
+			{
+				sum = sum + j;
+			}
+			next = j + k;
+			j = k;
+			k = next;
+		}
+
+
+		printf("%lu\n", sum);
+
+
+		return (0);
+	}
