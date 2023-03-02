7ac14b685cc521289057c1bf3845547cf4a56a2a
Natnael Hailu <122819655+nati673@users.noreply.github.com>
2023-03-02 04:08:33 -0800

    Update 104-print_buffer.c

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -1,44 +1,52 @@
-#include <stdio.h>
-#include "holberton.h"
-
-#define BUFFER_SIZE 10
-
-void print_buffer(char *b, int size)
-{
-    int i, j;
-
-    for (i = 0; i < size; i += BUFFER_SIZE) {
-        printf("%08x: ", i);
-        for (j = i; j < i + BUFFER_SIZE; j += 2) {
-            if (j < size) {
-                printf("%02x%02x", b[j], b[j + 1]);
-            } else {
-                printf("     ");
-            }
-            printf(" ");
-        }
-        for (j = i; j < i + BUFFER_SIZE; j++) {
-            if (j >= size) {
-                break;
-            }
-            if (b[j] >= 32 && b[j] <= 126) {
-                printf("%c", b[j]);
-            } else {
-                printf(".");
-            }
-        }
-        printf("\n");
-    }
-}
-
-int main(void)
-{
-    char buffer[98] = "This is a string!\nAnd this is the rest of the #buffer :)\n";
-    char buffer2[98] = "This is a string!\nAnd this is the rest of the #buffer :)\n";
-    int size = sizeof(buffer);
-
-    print_buffer(buffer, size);
-    printf("---------------------------------\n");
-    printf("%s", buffer2);
-    return (0);
+#include "main.h"                                                                                                                       
+#include <stdio.h>                                                                                                                      
+                                                                                                                                        
+/**                                                                                                                                     
+ * print_buffer - Prints a buffer 10 bytes at a time, starting with                                                                     
+ *                the byte position, then showing the hex content,                                                                      
+ *                then displaying printable charcaters.                                                                                 
+ * @b: The buffer to be printed.                                                                                                        
+ * @size: The number of bytes to be printed from the buffer.                                                                            
+ */                                                                                                                                     
+void print_buffer(char *b, int size)                                                                                                    
+{                                                                                                                                       
+        int byte, index;
+        
+        for (byte = 0; byte < size; byte += 10)                                                                                         
+        {                                                                                                                               
+                printf("%08x: ", byte);                                                                                                 
+                                                                                                                                        
+                for (index = 0; index < 10; index++)                                                                                    
+                {                                                                                                                       
+                        if ((index + byte) >= size)                                                                                     
+                                printf("  ");                                                                                           
+                                                                                                                                        
+                        else                                                                                                            
+                                printf("%02x", *(b + index + byte));                                                                    
+                                                                                                                                        
+                        if ((index % 2) != 0 && index != 0)                                                                             
+                                printf(" ");                                                                                            
+                }                                                                                                                       
+                                                                                                                                        
+                for (index = 0; index < 10; index++)                                                                                    
+                {                                                                                                                       
+                        if ((index + byte) >= size)                                                                                     
+                                break;                                                                                                  
+                                                                                                                                        
+                        else if (*(b + index + byte) >= 31 &&                                                                           
+                                 *(b + index + byte) <= 126)                                                                            
+                                printf("%c", *(b + index + byte));                                                                      
+                                                                                                                                        
+                        else                                                                                                            
+                                printf(".");                                                                                            
+                }                                                                                                                       
+                                                                                                                                        
+                if (byte >= size)                                                                                                       
+                        continue;                                                                                                       
+                                                                                                                                        
+                printf("\n");                                                                                                           
+        }                                                                                                                               
+                                                                                                                                        
+        if (size <= 0)                                                                                                                  
+                printf("\n");                                                                                                           
 }
