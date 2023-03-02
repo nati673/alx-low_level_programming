3226f7fbebad0002db418c97d3c99f4382e28627
Natnael Hailu <122819655+nati673@users.noreply.github.com>
2023-03-02 04:46:34 -0800

    Update 101-print_number.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,23 +1,23 @@
 #include "main.h"
 
 /**
- * print_number - prints an integer
- * @n: the integer to print
+ * print_number - print numbers chars
+ * @n: integer params
+ * Return: 0
  */
 void print_number(int n)
 {
-    int divisor = 1;
+	unsigned int n1;
 
-    if (n < 0) {
-        _putchar('-');
-        n = -n;
-    }
-
-    while (n / divisor >= 10)
-        divisor *= 10;
-
-    while (divisor != 0) {
-        _putchar((n / divisor) % 10 + '0');
-        divisor /= 10;
-    }
+	n1 = n;
+	if (n < 0)
+	{
+		_putchar('-');
+		n1 = -n;
+	}
+	if (n1 / 10 != 0)
+	{
+		print_number(n1 / 10);
+	}
+	_putchar((n1 % 10) + '0');
 }
