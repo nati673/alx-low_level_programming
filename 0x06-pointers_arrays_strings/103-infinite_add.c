07a2df6a3abc16ede6ca42410c32d3d4b48e3ebc
Natnael Hailu <122819655+nati673@users.noreply.github.com>
2023-03-02 01:19:15 -0800

    Update 103-infinite_add.c

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,48 +1,53 @@
 #include "main.h"
-#include <stdio.h>
+#include <string.h>
 
 /**
  * infinite_add - adds two numbers
- * @n1: number one.
- * @n2: number two.
- * @r: buffer that the function will use to store the result.
- * @size_r: buffer size:
- * Return: the pointer to dest.
+ * @n1: first number
+ * @n2: second number
+ * @r: buffer to store the result
+ * @size_r: size of buffer
+ *
+ * Return: pointer to the result, or 0 if the result cannot be stored in r
  */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int i, j, k, l, m, n;
+        int len1 = strlen(n1);
+        int len2 = strlen(n2);
+        int len = len1 > len2 ? len1 : len2;
+        int carry = 0;
+        int i, j;
 
-	for (i = 0; n1[i]; i++)
-		;
-	for (j = 0; n2[j]; j++)
-		;
-	if (i > size_r || j > size_r)
-		return (0);
-	m = 0;
-	for (i -= 1, j -= 1, k = 0; k < size_r - 1; i--, j--, k++)
-	{
-		n = m;
-		if (i >= 0)
-			n += n1[i] - '0';
-		if (j >= 0)
-			n += n2[j] - '0';
-		if (i < 0 && j < 0 && n == 0)
-		{
-			break;
-		}
-		m = n / 10;
-		r[k] = n % 10 + '0';
-	}
-	r[k] = '\0';
-	if (i >= 0 || j >= 0 || m)
-		return (0);
-	for (k -= 1, l = 0; l < k; k--, l++)
-	{
-		m = r[k];
-		r[k] = r[l];
-		r[l] = m;
-	}
-	return (r);
+        if (len + 1 > size_r)
+                return (0);
 
+        r[len + 1] = '\0';
+        for (i = len1 - 1, j = len2 - 1, len = len + 1; len > 0; i--, j--, len--)
+        {
+                int digit1 = i >= 0 ? n1[i] - '0' : 0;
+                int digit2 = j >= 0 ? n2[j] - '0' : 0;
+                int sum = digit1 + digit2 + carry;
+                if (sum >= 10)
+                {
+                        carry = 1;
+                        sum -= 10;
+                }
+                else
+                {
+                        carry = 0;
+                }
+                r[len - 1] = sum + '0';
+        }
+
+        if (r[0] == '0')
+        {
+                for (i = 0; r[i]; i++)
+                        r[i] = r[i + 1];
+        }
+        else if (carry == 1)
+        {
+                return (0);
+        }
+
+        return (r);
 }
